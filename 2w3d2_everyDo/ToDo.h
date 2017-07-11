e739f94458cf0887eeca4dd36f4b3ca9621cada4
hkimca <harrison.kim.ca@gmail.com>
2017-07-11 12:39:54 -0400

    initial commit

---
+++ 2w3d2_everyDo/ToDo.h
@@ -0,0 +1,15 @@
+//
+//  ToDo.h
+//  2w3d2_everyDo
+//
+//  Created by Seantastic31 on 11/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDo : NSObject
+
+@property (strong, nonatomic)
+
+@end
---
+++ 2w3d2_everyDo/ToDo.m
@@ -0,0 +1,13 @@
+//
+//  ToDo.m
+//  2w3d2_everyDo
+//
+//  Created by Seantastic31 on 11/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import "ToDo.h"
+
+@implementation ToDo
+
+@end
